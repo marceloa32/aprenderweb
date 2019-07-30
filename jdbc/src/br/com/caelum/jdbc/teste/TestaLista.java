eb568778d828e6aaf1b262d0c15022edb02927ab
Marcelo Augusto <marcelo.augusto@gzsistemas.com.br>
2019-07-30 17:55:38 -0300

    FJ-21
    aula 1 - exercicio 2.15-1

--- jdbc/src/br/com/caelum/jdbc/teste/TestaLista.java
+++ jdbc/src/br/com/caelum/jdbc/teste/TestaLista.java
@@ -1,5 +1,6 @@
 package br.com.caelum.jdbc.teste;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import br.com.caelum.jdbc.dao.ContatoDAO;
@@ -7,7 +8,8 @@ import br.com.caelum.jdbc.modelo.Contato;
 
 /**
  * FJ-21 14.5.2 exr. 2.13
- *
+ * exr 2.15 1
+ * 
  */
 public class TestaLista {
 	
@@ -16,11 +18,14 @@ public class TestaLista {
 		ContatoDAO dao = new ContatoDAO();
 		List<Contato> lista = dao.getLista();
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		for (Contato contato : lista) {
 			System.out.println("Nome: " + contato.getNome());
 			System.out.println("Email: " + contato.getEmail());
 			System.out.println("Endereco: " + contato.getEndereco());
-			System.out.println("Data de nascimento: " + contato.getDataNascimento().getTime());
+			
+			System.out.println("Data de nascimento: " + sdf.format(contato.getDataNascimento().getTime()));
 		}
 	}
 
