3eacd8b3e28c9749023a80f515bfdccc35234f22
Marcelo Augusto <marcelo.32@gmail.com>
2019-08-14 11:58:29 -0300

    FJ-21
    Exercicio 9.7

---
+++ fj21-agenda/WebContent/primeira-logica.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>
+Primeira lógica
+</title>
+<body>
+	<!-- view -->
+	<h1>Página da nossa primeira lógica</h1>
+</body>
+</html>
---
+++ fj21-agenda/src/br/com/caelum/mvc/logica/Logica.java
@@ -0,0 +1,8 @@
+package br.com.caelum.mvc.logica;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Logica {
+	void executa(HttpServletRequest req, HttpServletResponse resp) throws Exception;
+}
